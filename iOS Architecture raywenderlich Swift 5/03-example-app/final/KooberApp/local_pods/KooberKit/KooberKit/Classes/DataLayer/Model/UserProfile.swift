3f03cb01ae5a8074bd248869ca7e5fa5c32eccb8
JustinLau <janstilau@gmail.com>
2022-03-24 22:04:57 +0800

    sync

--- iOS Architecture raywenderlich Swift 5/03-example-app/final/KooberApp/local_pods/KooberKit/KooberKit/Classes/DataLayer/Model/UserProfile.swift
+++ iOS Architecture raywenderlich Swift 5/03-example-app/final/KooberApp/local_pods/KooberKit/KooberKit/Classes/DataLayer/Model/UserProfile.swift
@@ -1,46 +1,22 @@
-/// Copyright (c) 2018 Razeware LLC
-///
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to deal
-/// in the Software without restriction, including without limitation the rights
-/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/// copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
-///
-/// The above copyright notice and this permission notice shall be included in
-/// all copies or substantial portions of the Software.
-///
-/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
-/// distribute, sublicense, create a derivative work, and/or sell copies of the
-/// Software in any work that is designed, intended, or marketed for pedagogical or
-/// instructional purposes related to programming, coding, application development,
-/// or information technology.  Permission for such use, copying, modification,
-/// merger, publication, distribution, sublicensing, creation of derivative works,
-/// or sale is expressly withheld.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-/// THE SOFTWARE.
-
 import Foundation
 
+/*
+ 一个纯数据类, 可以参考一下代码的组织方式.
+ 和自己的代码组织方式很像. 一个类型下面, 空行用来进行分割.
+ */
 public struct UserProfile: Equatable, Codable {
-
-  // MARK: - Properties
-  public let name: String
-  public let email: String
-  public let mobileNumber: String
-  public let avatar: URL
-
-  // MARK: - Methods
-  public init(name: String, email: String, mobileNumber: String, avatar: URL) {
-    self.name = name
-    self.email = email
-    self.mobileNumber = mobileNumber
-    self.avatar = avatar
-  }
+    
+    // MARK: - Properties
+    public let name: String
+    public let email: String
+    public let mobileNumber: String
+    public let avatar: URL
+    
+    // MARK: - Methods
+    public init(name: String, email: String, mobileNumber: String, avatar: URL) {
+        self.name = name
+        self.email = email
+        self.mobileNumber = mobileNumber
+        self.avatar = avatar
+    }
 }
--- iOS Architecture raywenderlich Swift 5/03-example-app/final/KooberApp/local_pods/KooberKit/KooberKit/Classes/DataLayer/Repositories/UserSessionRepository.swift
+++ iOS Architecture raywenderlich Swift 5/03-example-app/final/KooberApp/local_pods/KooberKit/KooberKit/Classes/DataLayer/Repositories/UserSessionRepository.swift
@@ -1,6 +1,11 @@
 import Foundation
 import PromiseKit
 
+/*
+ 和自己的习惯一样, 类型下面的空行是必须的.
+ 用户数据相关的操作. 相比较同步获取的方式, 全部都是异步回调的方式.
+ 如果, 不使用 Promise 的方式, 那么这个接口就全部都是闭包传递的方式了.
+ */
 public protocol UserSessionRepository {
     
     func readUserSession() -> Promise<UserSession?>
