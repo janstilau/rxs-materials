d2c868a65e12f3af59f9aac0ffd95382ba4411b6
JustinLau <janstilau@gmail.com>
2022-03-26 01:01:17 +0800

    sync

--- 14-error-handling-in-practice/projects/final/Wundercast/Controllers/ApiController.swift
+++ 14-error-handling-in-practice/projects/final/Wundercast/Controllers/ApiController.swift
@@ -5,13 +5,16 @@ import CoreLocation
 import MapKit
 
 class ApiController {
+    
     struct Weather: Decodable {
+        
         let cityName: String
         let temperature: Int
         let humidity: Int
         let icon: String
         let coordinate: CLLocationCoordinate2D
         
+        // 提前定义的静态值, 在 error 发生, 或者起始状态可以直接使用.
         static let empty = Weather(
             cityName: "Unknown",
             temperature: -1000,
@@ -46,9 +49,14 @@ class ApiController {
             let info = try values.decode([AdditionalInfo].self, forKey: .weather)
             icon = iconNameToChar(icon: info.first?.icon ?? "")
             
+            // 这里体现了, nestedContainer 的用法.
+            // 那就是不想重新定义一个数据结构, 来配合 JSON 中的结构.
+            // 可以手动的进入到下一个层级, 然后将下一个层级中的数据, 解析到当前等级来.
+            // 第一次见到这样的方式.
             let mainInfo = try values.nestedContainer(keyedBy: MainKeys.self, forKey: .main)
             temperature = Int(try mainInfo.decode(Double.self, forKey: .temp))
             humidity = try mainInfo.decode(Int.self, forKey: .humidity)
+            
             let coordinate = try values.decode(Coordinate.self, forKey: .coordinate)
             self.coordinate = CLLocationCoordinate2D(latitude: coordinate.lat, longitude: coordinate.lon)
         }
@@ -87,8 +95,9 @@ class ApiController {
     /// The shared instance
     static var shared = ApiController()
     
-    /// The api key to communicate with openweathermap.org
-    /// Create your own on https://home.openweathermap.org/users/sign_up
+    
+    // 从这里可以看到, 其实可以将 BehaviorSubject 这种类型的 Subject, 当做成员变量来用的.
+    // 不可能, 所有的都是响应式. 所以, 一定要有指令式的代码.
     let apiKey = BehaviorSubject(value: "197d815bb621986c704668dde0b27e5d")
     
     /// API base URL
@@ -103,6 +112,9 @@ class ApiController {
     // MARK: - Api Calls
     func currentWeather(city: String) -> Observable<Weather> {
         return buildRequest(pathComponent: "weather", params: [("q", city)])
+        // buildRequest, 已经将 Session 的 Observable<(response: HTTPURLResponse, data: Data)>
+        // 变化成为了 Observable<Data>
+        // 所以添加节点的时候, 直接从 Data 入手其实就可以了.
             .map { data in
                 let decoder = JSONDecoder()
                 return try decoder.decode(Weather.self, from: data)
@@ -112,21 +124,29 @@ class ApiController {
     func currentWeather(at coordinate: CLLocationCoordinate2D) -> Observable<Weather> {
         return buildRequest(pathComponent: "weather", params: [("lat", "\(coordinate.latitude)"),
                                                                ("lon", "\(coordinate.longitude)")])
-        .map { data in
-            let decoder = JSONDecoder()
-            return try decoder.decode(Weather.self, from: data)
-        }
+            .map { data in
+                let decoder = JSONDecoder()
+                return try decoder.decode(Weather.self, from: data)
+            }
     }
     
     // MARK: - Private Methods
     
-    /**
+    /*
      * Private method to build a request with RxCocoa
      */
-    private func buildRequest(method: String = "GET", pathComponent: String, params: [(String, String)]) -> Observable<Data> {
+    // 同原有的代码组织结构一样, 还是可以编写, 返回通用的数据结构的方法.
+    // 然后, 在这个方法的基础上, 进行后续逻辑的处理. 因为, rx 是一种线性的结构, 他包装的应该是返回一种通用的数据结构, 然后交给后面进行变化.
+    private func buildRequest(method: String = "GET",
+                              pathComponent: String,
+                              params: [(String, String)]) -> Observable<Data> {
+        
+        // 这里其实就是一个构建 Request 的过程.
+        // 但是我们想的是, 把这个过程, 变为事件序列的一部分.
         let request: Observable<URLRequest> = Observable.create { observer in
             let url = self.baseURL.appendingPathComponent(pathComponent)
             var request = URLRequest(url: url)
+            
             let keyQueryItem = URLQueryItem(name: "appid", value: try? self.apiKey.value())
             let unitsQueryItem = URLQueryItem(name: "units", value: "metric")
             let urlComponents = NSURLComponents(url: url, resolvingAgainstBaseURL: true)!
@@ -138,7 +158,6 @@ class ApiController {
                 urlComponents.queryItems = queryItems
             } else {
                 urlComponents.queryItems = [keyQueryItem, unitsQueryItem]
-                
                 let jsonData = try! JSONSerialization.data(withJSONObject: params, options: .prettyPrinted)
                 request.httpBody = jsonData
             }
@@ -148,6 +167,9 @@ class ApiController {
             
             request.setValue("application/json", forHTTPHeaderField: "Content-Type")
             
+            // 其实, 将 Request 的构建构成, 暴露在外面, 然后 Observable.create 使用这个捕获的 Request 也是没有问题的.
+            // 但现在的这种代码方式, 更加的合理和清晰, 将事件的源头仅仅的包裹.
+            // 后续的代码, 直接拿到的就是响应式的代码结构.
             observer.onNext(request)
             observer.onCompleted()
             
@@ -156,16 +178,30 @@ class ApiController {
         
         let session = URLSession.shared
         
+        // 调用 FlatMap --> 当上游的数据节点达到之后, 触发 FlatMap 里面的事件序列的逻辑.
+        // 然后后续的节点, 其实是监听的这个生成的事件序列的值.
+        // flatMap 很像是, Promise 的 then.
+        
+        /*
+         FlatMap 的处理逻辑, 是将生成的事件序列, 调用 subscribe 注册到一个中间节点, 中间节点将生成的事件序列的值, 传递给 flatMap 的后续节点.
+         所以 flatMap 中的事件序列, 只有到上游节点来临时, 才会真正的产生数据源.
+         */
         return request.flatMap { request in
             
+            // Observable<(response: HTTPURLResponse, data: Data)>
+            // session.rx.response --> Observable<(response: HTTPURLResponse, data: Data)>
+            // rx 官方包装的 resposne 是这样的一个序列.
             return session.rx.response(request: request)
                 .map { response, data in
+                    // 在这里的处理是, 根据 code 的值, 进行了 throw 的处理
+                    // 而之所以可以进行 throw, 是因为 map 里面, 接受到的是一个 throw 的 Block
                     switch response.statusCode {
                     case 200 ..< 300:
                         return data
                     case 401:
                         throw ApiError.invalidKey
                     case 400 ..< 500:
+                        // 在熟知了服务器端逻辑的情况下, 才能写出这样的代码出来. 
                         throw ApiError.cityNotFound
                     default:
                         throw ApiError.serverFailure
@@ -173,10 +209,9 @@ class ApiController {
                 }
         }
     }
-    
 }
 
-/**
+/*
  * Maps an icon information from the API to a local char
  * Source: http://openweathermap.org/weather-conditions
  */
--- 14-error-handling-in-practice/projects/final/Wundercast/Extensions/RxReachability.swift
+++ 14-error-handling-in-practice/projects/final/Wundercast/Extensions/RxReachability.swift
@@ -1,109 +1,84 @@
-/// Copyright (c) 2020 Razeware LLC
-/// 
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to deal
-/// in the Software without restriction, including without limitation the rights
-/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/// copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
-/// 
-/// The above copyright notice and this permission notice shall be included in
-/// all copies or substantial portions of the Software.
-/// 
-/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
-/// distribute, sublicense, create a derivative work, and/or sell copies of the
-/// Software in any work that is designed, intended, or marketed for pedagogical or
-/// instructional purposes related to programming, coding, application development,
-/// or information technology.  Permission for such use, copying, modification,
-/// merger, publication, distribution, sublicensing, creation of derivative works,
-/// or sale is expressly withheld.
-/// 
-/// This project and source code may use libraries or frameworks that are
-/// released under various Open-Source licenses. Use of those libraries and
-/// frameworks are governed by their own individual licenses.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-/// THE SOFTWARE.
-
 import SystemConfiguration
 import Foundation
 import RxSwift
 import RxCocoa
 
 enum Reachability {
-  case offline
-  case online
-  case unknown
-
-  init(reachabilityFlags flags: SCNetworkReachabilityFlags) {
-    let connectionRequired = flags.contains(.connectionRequired)
-    let isReachable = flags.contains(.reachable)
-
-    if !connectionRequired && isReachable {
-      self = .online
-    } else {
-      self = .offline
+    case offline
+    case online
+    case unknown
+    
+    init(reachabilityFlags flags: SCNetworkReachabilityFlags) {
+        let connectionRequired = flags.contains(.connectionRequired)
+        let isReachable = flags.contains(.reachable)
+        
+        if !connectionRequired && isReachable {
+            self = .online
+        } else {
+            self = .offline
+        }
     }
-  }
 }
 
 class RxReachability {
-  static let shared = RxReachability()
-
-  private init() {}
-
-  private static var _status = BehaviorRelay<Reachability>(value: .unknown)
-  var status: Observable<Reachability> {
-    return RxReachability._status.asObservable().distinctUntilChanged()
-  }
-
-  class func reachabilityStatus() -> Reachability {
-    return RxReachability._status.value
-  }
-
-  func isOnline() -> Bool {
-    switch RxReachability._status.value {
-    case .online:
-      return true
-    case .offline, .unknown:
-      return false
+    static let shared = RxReachability()
+    
+    private init() {}
+    
+    // 网络状态, 是一个应该存储的值.
+    // 当新的 Observer 来进行监听的时候, 可以给他当前的状态值.
+    private static var _status = BehaviorRelay<Reachability>(value: .unknown)
+    
+    // 不给外界当前的状态, 仅仅是暴露出一个 Observable<Reachability> 抽象数据类型.
+    var status: Observable<Reachability> {
+        return RxReachability._status.asObservable().distinctUntilChanged()
     }
-  }
-
-  private var reachability: SCNetworkReachability?
-
-  func startMonitor(_ host: String) -> Bool {
-    guard reachability != nil else {
-      return true
+    
+    // 类方法, 就是全局数据的操作方法.
+    // 这里直接就是读取的 BehaviorRelay 里面的值.
+    // 再次证明了, BehaviorSubject 应该当做是变量来进行使用.
+    class func reachabilityStatus() -> Reachability {
+        return RxReachability._status.value
     }
-
-    var context = SCNetworkReachabilityContext(version: 0, info: nil, retain: nil, release: nil, copyDescription: nil)
-
-    if let reachability = SCNetworkReachabilityCreateWithName(nil, host) {
-      SCNetworkReachabilitySetCallback(reachability, { (_, flags, _) in
-        let status = Reachability(reachabilityFlags: flags)
-        RxReachability._status.accept(status)
-      }, &context)
-
-      SCNetworkReachabilityScheduleWithRunLoop(reachability, CFRunLoopGetMain(), CFRunLoopMode.commonModes.rawValue)
-      self.reachability = reachability
-
-      return true
+    
+    func isOnline() -> Bool {
+        switch RxReachability._status.value {
+        case .online:
+            return true
+        case .offline, .unknown:
+            return false
+        }
     }
-
-    return true
-  }
-
-  func stopMonitor() {
-    if let _reachability = reachability {
-      SCNetworkReachabilityUnscheduleFromRunLoop(_reachability, CFRunLoopGetMain(), CFRunLoopMode.commonModes.rawValue);
-      reachability = nil
+    
+    private var reachability: SCNetworkReachability?
+    
+    func startMonitor(_ host: String) -> Bool {
+        guard reachability != nil else {
+            return true
+        }
+        
+        var context = SCNetworkReachabilityContext(version: 0, info: nil, retain: nil, release: nil, copyDescription: nil)
+        
+        // 无论, 监听的方法如何底层. 但是只要用 Subject 进行监听, 就可以让这个值, 变为事件序列.
+        if let reachability = SCNetworkReachabilityCreateWithName(nil, host) {
+            SCNetworkReachabilitySetCallback(reachability, { (_, flags, _) in
+                let status = Reachability(reachabilityFlags: flags)
+                RxReachability._status.accept(status)
+            }, &context)
+            
+            SCNetworkReachabilityScheduleWithRunLoop(reachability, CFRunLoopGetMain(), CFRunLoopMode.commonModes.rawValue)
+            self.reachability = reachability
+            return true
+        }
+        
+        return true
     }
-  }
-
+    
+    func stopMonitor() {
+        if let _reachability = reachability {
+            SCNetworkReachabilityUnscheduleFromRunLoop(_reachability, CFRunLoopGetMain(), CFRunLoopMode.commonModes.rawValue);
+            reachability = nil
+        }
+    }
+    
 }
