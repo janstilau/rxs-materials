5483efa4a5f357cd896cb06883630a3cde04a522
liuguoqiang <liugq01@rd.netease.com>
2022-03-26 16:52:57 +0800

    sync

--- 15-intro-to-schedulers/projects/final/Schedulers/main.swift
+++ 15-intro-to-schedulers/projects/final/Schedulers/main.swift
@@ -1,35 +1,3 @@
-/// Copyright (c) 2020 Razeware LLC
-/// 
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to deal
-/// in the Software without restriction, including without limitation the rights
-/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/// copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
-/// 
-/// The above copyright notice and this permission notice shall be included in
-/// all copies or substantial portions of the Software.
-/// 
-/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
-/// distribute, sublicense, create a derivative work, and/or sell copies of the
-/// Software in any work that is designed, intended, or marketed for pedagogical or
-/// instructional purposes related to programming, coding, application development,
-/// or information technology.  Permission for such use, copying, modification,
-/// merger, publication, distribution, sublicensing, creation of derivative works,
-/// or sale is expressly withheld.
-/// 
-/// This project and source code may use libraries or frameworks that are
-/// released under various Open-Source licenses. Use of those libraries and
-/// frameworks are governed by their own individual licenses.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-/// THE SOFTWARE.
-
 import Foundation
 import RxSwift
 
@@ -40,54 +8,98 @@ let bag = DisposeBag()
 let animal = BehaviorSubject(value: "[dog]")
 
 animal
-  .subscribeOn(MainScheduler.instance)
-  .dump()
-  .observeOn(globalScheduler)
-  .dumpingSubscription()
-  .disposed(by: bag)
+// subscribeOn 的作用, 其实是将 Publisher 的 Subscribe 方法, 在特定的 schedule 上进行触发 subscribe 的操作.
+    .subscribeOn(MainScheduler.instance)
+// dump 就是使用 do 进行进行后续的操作.
+    .dump()
+// observeOn 则是 on 事件处理应该在哪里进行进行.
+    .observeOn(globalScheduler)
+// dumpingSubscription 是触发整个事件序列的开始 .
+    .dumpingSubscription()
+    .disposed(by: bag)
 
 let fruit = Observable<String>.create { observer in
-  observer.onNext("[apple]")
-  sleep(2)
-  observer.onNext("[pineapple]")
-  sleep(2)
-  observer.onNext("[strawberry]")
-  return Disposables.create()
+    observer.onNext("[apple]")
+    sleep(2)
+    observer.onNext("[pineapple]")
+    sleep(2)
+    observer.onNext("[strawberry]")
+    return Disposables.create()
 }
 
 fruit
-  .subscribeOn(globalScheduler)
-  .dump()
-  .observeOn(MainScheduler.instance)
-  .dumpingSubscription()
-  .disposed(by: bag)
+    .subscribeOn(globalScheduler)
+    .dump()
+    .observeOn(MainScheduler.instance)
+    .dumpingSubscription()
+    .disposed(by: bag)
 
+// 和 OC 的 target action 模式相比, 这种直接使用命令对象开启一个线程其实要好的多.
+// 首先不用考虑内存问题, 使用闭包, 自动有着内存管理.
+// 再者, 逻辑也更加的清晰, 之前还要专门定义一个类, 这个类里面的一个启动方法. 这些都是要暴露出去的, 现在直接使用一个闭包就可以了, 而闭包是可以随意进行传递的. 可以在 Thread 初始化的时候, 进行设定.
 let animalsThread = Thread() {
-  sleep(3)
-  animal.onNext("[cat]")
-  sleep(3)
-  animal.onNext("[tiger]")
-  sleep(3)
-  animal.onNext("[fox]")
-  sleep(3)
-  animal.onNext("[leopard]")
+    sleep(3)
+    animal.onNext("[cat]")
+    sleep(3)
+    animal.onNext("[tiger]")
+    sleep(3)
+    animal.onNext("[fox]")
+    sleep(3)
+    animal.onNext("[leopard]")
 }
 
 animalsThread.name = "Animals Thread"
 animalsThread.start()
 
-
 animal.subscribeOn(MainScheduler.instance)
-  .dump()
-  .observeOn(globalScheduler)
-  .dumpingSubscription()
-  .disposed(by:bag)
+    .dump()
+    .observeOn(globalScheduler)
+    .dumpingSubscription()
+    .disposed(by:bag)
 
 fruit.subscribeOn(globalScheduler)
-  .dump()
-  .observeOn(MainScheduler.instance)
-  .dumpingSubscription()
-  .disposed(by:bag)
+    .dump()
+    .observeOn(MainScheduler.instance)
+    .dumpingSubscription()
+    .disposed(by:bag)
 
-RunLoop.main.run(until: Date(timeIntervalSinceNow: 13))
+RunLoop.main.run(until: Date(timeIntervalSinceNow: 20))
 
+/*
+ 
+ ===== Schedulers =====
+
+ 00s | [E] [dog] emitted on Main Thread
+ 00s | [S] [dog] received on Anonymous Thread
+ 00s | [E] [dog] emitted on Main Thread
+ 00s | [E] [apple] emitted on Anonymous Thread
+ 00s | [S] [dog] received on Anonymous Thread
+ 
+ 00s | [E] [apple] emitted on Anonymous Thread
+ 00s | [S] [apple] received on Main Thread
+ 00s | [S] [apple] received on Main Thread
+ 02s | [E] [pineapple] emitted on Anonymous Thread
+ 02s | [E] [pineapple] emitted on Anonymous Thread
+ 02s | [S] [pineapple] received on Main Thread
+ 02s | [S] [pineapple] received on Main Thread
+ 03s | [E] [cat] emitted on Animals Thread
+ 03s | [S] [cat] received on Anonymous Thread
+ 03s | [E] [cat] emitted on Animals Thread
+ 03s | [S] [cat] received on Anonymous Thread
+ 04s | [E] [strawberry] emitted on Anonymous Thread
+ 04s | [E] [strawberry] emitted on Anonymous Thread
+ 04s | [S] [strawberry] received on Main Thread
+ 04s | [S] [strawberry] received on Main Thread
+ 06s | [E] [tiger] emitted on Animals Thread
+ 06s | [E] [tiger] emitted on Animals Thread
+ 06s | [S] [tiger] received on Anonymous Thread
+ 06s | [S] [tiger] received on Anonymous Thread
+ 09s | [E] [fox] emitted on Animals Thread
+ 09s | [E] [fox] emitted on Animals Thread
+ 09s | [S] [fox] received on Anonymous Thread
+ 09s | [S] [fox] received on Anonymous Thread
+ 12s | [E] [leopard] emitted on Animals Thread
+ 12s | [E] [leopard] emitted on Animals Thread
+ 12s | [S] [leopard] received on Anonymous Thread
+ 12s | [S] [leopard] received on Anonymous Thread
+ */
