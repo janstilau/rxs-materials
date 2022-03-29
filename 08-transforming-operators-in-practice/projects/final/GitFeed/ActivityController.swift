d11bf5e42c6b72f3bdd07912e677132c0519933b
JustinLau <janstilau@gmail.com>
2022-03-29 16:56:39 +0800

    sync

--- 08-transforming-operators-in-practice/projects/final/GitFeed/ActivityController.swift
+++ 08-transforming-operators-in-practice/projects/final/GitFeed/ActivityController.swift
@@ -1,35 +1,3 @@
-/// Copyright (c) 2020 Razeware LLC
-/// 
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to deal
-/// in the Software without restriction, including without limitation the rights
-/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/// copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
-/// 
-/// The above copyright notice and this permission notice shall be included in
-/// all copies or substantial portions of the Software.
-/// 
-/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
-/// distribute, sublicense, create a derivative work, and/or sell copies of the
-/// Software in any work that is designed, intended, or marketed for pedagogical or
-/// instructional purposes related to programming, coding, application development,
-/// or information technology.  Permission for such use, copying, modification,
-/// merger, publication, distribution, sublicensing, creation of derivative works,
-/// or sale is expressly withheld.
-/// 
-/// This project and source code may use libraries or frameworks that are
-/// released under various Open-Source licenses. Use of those libraries and
-/// frameworks are governed by their own individual licenses.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-/// THE SOFTWARE.
-
 import UIKit
 import RxSwift
 import RxCocoa
@@ -44,10 +12,10 @@ func cachedFileURL(_ fileName: String) -> URL {
 
 class ActivityController: UITableViewController {
   private let repo = "ReactiveX/RxSwift"
-
+  
   private let events = BehaviorRelay<[Event]>(value: [])
   private let bag = DisposeBag()
-
+  
   private let eventsFileURL = cachedFileURL("events.json")
   private let modifiedFileURL = cachedFileURL("modified.txt")
   
@@ -56,70 +24,88 @@ class ActivityController: UITableViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     title = repo
-
+    
     self.refreshControl = UIRefreshControl()
     let refreshControl = self.refreshControl!
-
+    
     refreshControl.backgroundColor = UIColor(white: 0.98, alpha: 1.0)
     refreshControl.tintColor = UIColor.darkGray
     refreshControl.attributedTitle = NSAttributedString(string: "Pull to refresh")
     refreshControl.addTarget(self, action: #selector(refresh), for: .valueChanged)
-
+    
+    // 在界面出现的时候, 进行缓存数据的读取, 这样可以快速的进行界面的展示.
     let decoder = JSONDecoder()
     if let eventsData = try? Data(contentsOf: eventsFileURL),
        let persistedEvents = try? decoder.decode([Event].self, from: eventsData) {
       events.accept(persistedEvents)
     }
     
+    // 在界面的时候, 进行缓存数据的读取, 确保后续刷新可以在锚点之后进行.
     if let lastModifiedString = try? String(contentsOf: modifiedFileURL, encoding: .utf8) {
       lastModified.accept(lastModifiedString)
     }
     
     refresh()
   }
-
+  
   @objc func refresh() {
     DispatchQueue.global(qos: .default).async { [weak self] in
       guard let self = self else { return }
       self.fetchEvents(repo: self.repo)
     }
   }
-
+    
+  // 真正的网络交互的触发.
+  // 在这里大量的使用了 flatMap, 触发新的事件序列.
   func fetchEvents(repo: String) {
     let response = Observable.from([repo])
       .map { urlString -> URL in
+        // 使用 map, 得到真正的 URL 对象, 作为后面触发网络请求的 URL.
         return URL(string: "https://api.github.com/repos/\(urlString)/events")!
       }
+    // 使用 map, 将 URL 对象, 构建成为一个 URLRequest 对象.
       .map { [weak self] url -> URLRequest in
         var request = URLRequest(url: url)
+        // 这是那个锚点值, 根据这个值, 可以拉取列表某个点之后的数据.
         if let modifiedHeader = self?.lastModified.value {
           request.addValue(modifiedHeader,
-            forHTTPHeaderField: "Last-Modified")
+                           forHTTPHeaderField: "Last-Modified")
         }
         return request
       }
+    // 真正的触发网络请求的, 在 FlatMap 的内部.
       .flatMap { request -> Observable<(response: HTTPURLResponse, data: Data)> in
         return URLSession.shared.rx.response(request: request)
       }
       .share(replay: 1)
     
+    // response next 里面包裹的值, 是 (response: HTTPURLResponse, data: Data) 这个值.
     response
+    // 首先根据 Code 值, 来过滤一些错误的数据.
       .filter { response, _ in
         return 200..<300 ~= response.statusCode
       }
+    // 将 Data 部分, 构建成为 [Event] 的形式.
       .compactMap { _, data -> [Event]? in
         return try? JSONDecoder().decode([Event].self, from: data)
       }
+    // 将真正的网络得到的 Model 数据, 交给命令式的世界, 进行处理.
       .subscribe(onNext: { [weak self] newEvents in
         self?.processEvents(newEvents)
       })
       .disposed(by: bag)
     
+    // 同样的一个网络请求, 上方是真正的业务数据的处理.
+    // 这里是 Last-Modified 的更新.
+    // 信号发送注册的方式, 使得业务逻辑的处理, 按照自己的方式进行管理, 而不用所有的东西都写到一个地方.
     response
       .filter { response, _ in
         return 200..<400 ~= response.statusCode
       }
       .flatMap { response, _ -> Observable<String> in
+        // 这里使用 map 其实也可以达到要求, 后面增加一个 Filter 的 Operator 就可以.
+        // 不过, 使用 flatMap, 使用新的一个事件序列, 也是一个新的方式.
+        // 在 rx 里面, 很喜欢这种, trigger 
         guard let value = response.allHeaderFields["Last-Modified"] as? String else {
           return Observable.empty()
         }
@@ -127,7 +113,7 @@ class ActivityController: UITableViewController {
       }
       .subscribe(onNext: { [weak self] modifiedHeader in
         guard let self = self else { return }
-
+        
         self.lastModified.accept(modifiedHeader)
         try? modifiedHeader.write(to: self.modifiedFileURL, atomically: true, encoding: .utf8)
       })
@@ -139,27 +125,30 @@ class ActivityController: UITableViewController {
     if updatedEvents.count > 50 {
       updatedEvents = [Event](updatedEvents.prefix(upTo: 50))
     }
-
+    
+    // 真正的触发 dataSource 的更新操作.
+    // 然后进行 TableView 的刷新.
     events.accept(updatedEvents)
     DispatchQueue.main.async {
       self.tableView.reloadData()
       self.refreshControl?.endRefreshing()
     }
-
+    
+    // 然后, 对现在的数据进行缓存.
     let encoder = JSONEncoder()
     if let eventsData = try? encoder.encode(updatedEvents) {
       try? eventsData.write(to: eventsFileURL, options: .atomicWrite)
     }
   }
-
+  
   // MARK: - Table Data Source
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return events.value.count
   }
-
+  
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let event = events.value[indexPath.row]
-
+    
     let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")!
     cell.textLabel?.text = event.actor.name
     cell.detailTextLabel?.text = event.repo.name + ", " + event.action.replacingOccurrences(of: "Event", with: "").lowercased()
