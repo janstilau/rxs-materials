c6f3e2b54b6277755f0ffa0ea9e6d346e44aeb17
JustinLau <janstilau@gmail.com>
2022-03-30 01:15:53 +0800

    sync

--- 10-combining-operators-in-practice/projects/final/OurPlanet/OurPlanet/EventCell.swift
+++ 10-combining-operators-in-practice/projects/final/OurPlanet/OurPlanet/EventCell.swift
@@ -1,9 +1,12 @@
 import UIKit
 
+// 简单的一个 Cell 类, 可以看到, 通过 Model 进行 UI 刷新, 是一个常见的行为. 
 class EventCell : UITableViewCell {
+  
   @IBOutlet var title: UILabel!
   @IBOutlet var date: UILabel!
   @IBOutlet var details: UILabel!
+  
   
   func configure(event: EOEvent) {
     title.text = event.title
