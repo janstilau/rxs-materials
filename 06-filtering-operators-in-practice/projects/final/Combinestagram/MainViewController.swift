dbd6c4d21b4ff9d9dc9ee72783099ff358d643b3
JustinLau <janstilau@gmail.com>
2022-03-29 15:28:21 +0800

    sync

--- 06-filtering-operators-in-practice/projects/final/Combinestagram/MainViewController.swift
+++ 06-filtering-operators-in-practice/projects/final/Combinestagram/MainViewController.swift
@@ -1,63 +1,31 @@
-/// Copyright (c) 2020 Razeware LLC
-/// 
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to deal
-/// in the Software without restriction, including without limitation the rights
-/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/// copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
-/// 
-/// The above copyright notice and this permission notice shall be included in
-/// all copies or substantial portions of the Software.
-/// 
-/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
-/// distribute, sublicense, create a derivative work, and/or sell copies of the
-/// Software in any work that is designed, intended, or marketed for pedagogical or
-/// instructional purposes related to programming, coding, application development,
-/// or information technology.  Permission for such use, copying, modification,
-/// merger, publication, distribution, sublicensing, creation of derivative works,
-/// or sale is expressly withheld.
-/// 
-/// This project and source code may use libraries or frameworks that are
-/// released under various Open-Source licenses. Use of those libraries and
-/// frameworks are governed by their own individual licenses.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-/// THE SOFTWARE.
-
 import UIKit
 import RxSwift
 import RxRelay
 
 class MainViewController: UIViewController {
-
+  
   @IBOutlet weak var imagePreview: UIImageView!
   @IBOutlet weak var buttonClear: UIButton!
   @IBOutlet weak var buttonSave: UIButton!
   @IBOutlet weak var itemAdd: UIBarButtonItem!
-
+  
   private let bag = DisposeBag()
   private let images = BehaviorRelay<[UIImage]>(value: [])
-
+  
   private var imageCache = [Int]()
   
   override func viewDidLoad() {
     super.viewDidLoad()
-
+    
     images
       .throttle(.milliseconds(500), scheduler: MainScheduler.instance)
       .subscribe(onNext: { [weak imagePreview] photos in
         guard let preview = imagePreview else { return }
-
+        
         preview.image = photos.collage(size: preview.frame.size)
       })
       .disposed(by: bag)
-
+    
     images
       .subscribe(onNext: { [weak self] photos in
         self?.updateUI(photos: photos)
@@ -69,10 +37,10 @@ class MainViewController: UIViewController {
     images.accept([])
     imageCache = []
   }
-
+  
   @IBAction func actionSave() {
     guard let image = imagePreview.image else { return }
-
+    
     PhotoWriter.save(image)
       .subscribe(
         onSuccess: { [weak self] id in
@@ -85,15 +53,15 @@ class MainViewController: UIViewController {
       )
       .disposed(by: bag)
   }
-
+  
   @IBAction func actionAdd() {
     // images.accept(images.value + [UIImage(named: "IMG_1907.jpg")!])
-
+    
     let photosViewController = storyboard!.instantiateViewController(
       withIdentifier: "PhotosViewController") as! PhotosViewController
-
+    
     navigationController!.pushViewController(photosViewController, animated: true)
-
+    
     let newPhotos = photosViewController.selectedPhotos
       .share()
     
@@ -123,7 +91,7 @@ class MainViewController: UIViewController {
         }
       )
       .disposed(by: bag)
-
+    
     newPhotos
       .ignoreElements()
       .subscribe(onCompleted: { [weak self] in
@@ -131,14 +99,14 @@ class MainViewController: UIViewController {
       })
       .disposed(by: bag)
   }
-
+  
   private func updateNavigationIcon() {
     let icon = imagePreview.image?
       .scaled(CGSize(width: 22, height: 22))
       .withRenderingMode(.alwaysOriginal)
-      
+    
     navigationItem.leftBarButtonItem = UIBarButtonItem(image: icon,
-      style: .done, target: nil, action: nil)
+                                                       style: .done, target: nil, action: nil)
   }
   
   func showMessage(_ title: String, description: String? = nil) {
@@ -146,7 +114,7 @@ class MainViewController: UIViewController {
       .subscribe()
       .disposed(by: bag)
   }
-
+  
   private func updateUI(photos: [UIImage]) {
     buttonSave.isEnabled = photos.count > 0 && photos.count % 2 == 0
     buttonClear.isEnabled = photos.count > 0
